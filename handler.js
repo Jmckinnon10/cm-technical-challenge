67361345550ea8ab7a48bb4a4405598a5f324ac7
John McKinnon <johnmckinnon23@yahoo.com>
2023-04-26 08:04:18 -0600

    reverse

--- handler.js
+++ handler.js
@@ -1,1 +1,20 @@
 // write your lambda here
+
+
+module.exports.reverser = async (event) => {
+  const result = event.body.split('').reverse().join('')
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message:"Thank you for using the reverser!",
+        payload: result
+      },
+      null,
+      2
+    )
+  }
+}
+
+module.exports.reverser("hello")
